51c02eee37e345c61fbc662e6d06e9deae997d5f
Gabriel Calderon <gabrielvilleda8@gmail.com>
2024-06-08 18:41:41 -0600

    Setup AppWeb

---
+++ src/main/java/libcode/webapp/negocio/DataService.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package libcode.webapp.negocio;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import libcode.webapp.entidades.Alumno;
+import libcode.webapp.entidades.Materia;
+
+
+
+@Stateless
+public class DataService {
+    
+    @PersistenceContext(unitName = "pu")
+    
+    EntityManager entityManager;
+    
+    public List<Alumno> getAlumnos() 
+    {
+        Query query = entityManager.createQuery("SELECT e FROM Alumno e " + "ORDER BY e.id ASC");
+        List<Alumno> alumnos = query.getResultList();
+        return alumnos;
+    }
+    
+    public List<Materia> getMaterias() 
+    {
+        Query query = entityManager.createQuery("SELECT m FROM Materia m ORDER BY m.id ASC");
+        List<Materia> materias = query.getResultList();
+        return materias;
+    }
+    
+    @Transactional
+    public void saveAlumno(Alumno alumno)
+    {
+        entityManager.persist(alumno);
+    }
+    
+    @Transactional
+    public void editAlumno(Alumno alumno)
+    {
+        entityManager.merge(alumno);
+    }
+    
+    @Transactional
+    public void deleteAlumno(Alumno alumno)
+    {
+        Alumno alumnoEliminar = entityManager.find(Alumno.class,alumno.getId());
+        entityManager.remove(alumnoEliminar);
+    }
+    
+    @Transactional
+    public void saveMateria(Materia materia)
+    {
+        entityManager.persist(materia);
+    }
+    
+    @Transactional
+    public void editMateria(Materia materia)
+    {
+        entityManager.merge(materia);
+    }
+    
+    @Transactional
+    public void deleteMateria(Materia materia)
+    {
+        Materia materiaEliminar = entityManager.find(Materia.class, materia.getId());
+        entityManager.remove(materiaEliminar);
+    }
+    
+}
---
+++ src/main/java/libcode/webapp/resources/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package libcode.webapp.resources;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+
+
+/**
+ *
+ * @author Gabo
+ */
+@ApplicationPath("/api")
+public class App extends Application {
+    
+}
